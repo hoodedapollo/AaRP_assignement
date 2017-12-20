a228da86352f83bce6b9e4e1a8dc8e46efbc3350
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-20 11:38:19 +0100

    Add comments

--- secondAssign/Publisher1_2.h
+++ secondAssign/Publisher1_2.h
@@ -19,7 +19,7 @@ class Publisher {
                 void set_period(int); // set publishing period
                 void set_pipe_filedes(int, int); // set file descriptor attributes
                 void set_pipe_filedes(char*,char*); // if file des are chars convert them into int before setting the corresponding attributes
-                void write_in_pipe(char); // generate a random char and write it in the pipe at a given frequency
+                void write_in_pipe(char); // generate a random char and write it in the pipe once per period
 };
 
 Publisher::Publisher (int pub_period)
@@ -32,14 +32,14 @@ void Publisher::set_period(int pub_period)
         publishing_period = pub_period; // seconds
 }
 
-void Publisher::set_pipe_filedes( int filedes0, int filedes1)
+void Publisher::set_pipe_filedes( int filedes0, int filedes1) // take the pipe reading file descriptor (fildes0) and the writing one (fileds1), sets the attributes accordingly  and closes the reading one
 {
-        pipe_filedes_reading = filedes0;
-        pipe_filedes_writing = filedes1;
-        close(pipe_filedes_reading);
+        pipe_filedes_reading = filedes0; // sets attribute accordingly
+        pipe_filedes_writing = filedes1; // sets attribute accordingly
+        close(pipe_filedes_reading); // closes pipe reading file descriptor
 }
 
-void Publisher::set_pipe_filedes( char *filedes0, char *filedes1)
+void Publisher::set_pipe_filedes( char *filedes0, char *filedes1) // overload in case the file descriptors are type char
 {
         pipe_filedes_reading = atoi(filedes0);
         pipe_filedes_writing = atoi(filedes1);
@@ -48,7 +48,7 @@ void Publisher::set_pipe_filedes( char *filedes0, char *filedes1)
 
 
 
-void Publisher::write_in_pipe (char Aa)
+void Publisher::write_in_pipe (char Aa) // generate a random char, capitol letter or not depending on Aa, and write it in the pipe once per period
 {
         char randomletter;
         while(1)
