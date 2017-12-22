60719147de36d3d98ed4b20fead4abad6aa64432
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-22 12:54:48 +0100

    Add comments

--- secondAssign/auxiliary_functions.h
+++ secondAssign/auxiliary_functions.h
@@ -34,7 +34,7 @@ int max_positive_in_column_2D_array(int **table, int column) // return the maxim
         return max_fd;
 }
 
-void int_2Darray_dynamic_alloc(int ** table, int rows, int columns)
+void int_2Darray_dynamic_alloc(int ** table, int rows, int columns) // Dynamic allocate memory for a 2D array int** table with number of rows = int rows and number of colums = int colums 
 {
         table = new int*[rows];
                 for (int i = 0; i < rows; i++)
