0ccff3651c167066ecb334f99aff70c1f61bc9ba
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-22 15:08:54 +0100

    Bug fix

--- secondAssign/publisher_test/publisher_test_child1_0.cpp
+++ secondAssign/publisher_test/publisher_test_child1_0.cpp
@@ -6,13 +6,15 @@
 
 using namespace std;
 
+#define PUB_PERIOD 1 
+
 int main(int argc, char* argv[])
 {
         int filedes[2];
-        Publisher pub1(5,filedes[0],filedes[1]);
 
         filedes[0] = atoi(argv[1]);
         filedes[1] = atoi(argv[2]);
 
+        Publisher pub1(PUB_PERIOD,filedes[0],filedes[1]);
         pub1.write_in_pipe('a');
 }
