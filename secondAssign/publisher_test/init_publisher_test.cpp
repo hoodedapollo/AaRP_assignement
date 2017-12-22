f6e279fb54bc72d5b7836d1e05395df7ad7217b4
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-22 15:09:55 +0100

    Comment unecessary lines (debug)

--- secondAssign/publisher_test/init_publisher_test.cpp
+++ secondAssign/publisher_test/init_publisher_test.cpp
@@ -10,12 +10,12 @@ using namespace std;
 int main (int argc, char* argv[])
 {
         int filedes[2];
-        char char_filedes[2][4];
+        char char_filedes[2];
 
         pipe(filedes);
         
-        sprintf(char_filedes[0], "%d", filedes[0]); // convert a integer into a string of char (first row of 4 chars --> char_filedes[0] is the pointer to the first row array)
-        sprintf(char_filedes[1], "%d", filedes[1]); //
+        sprintf(&char_filedes[0], "%d", filedes[0]); // convert a integer into a string of char (first row of 4 chars --> char_filedes[0] is the pointer to the first row array)
+        sprintf(&char_filedes[1], "%d", filedes[1]); //
 
         pid_t fork_pid = fork();
 
@@ -27,8 +27,8 @@ int main (int argc, char* argv[])
         if (fork_pid == 0) // child process
         {
                 argv[0] = "publisher_test_child";
-                argv[1] = char_filedes[0];        
-                argv[2] = char_filedes[1];
+                *argv[1] = char_filedes[0];        
+                *argv[2] = char_filedes[1];
                 argv[3] = NULL;
 
                 execve("publisher_test_child", argv, NULL);
@@ -37,23 +37,23 @@ int main (int argc, char* argv[])
         if (fork_pid > 0) // father process
         {
                 char single_char;
-                int kbd_input;
-                fd_set standard_input;
-                FD_ZERO(&standard_input);
+//                int kbd_input;
+//                fd_set standard_input;
+//                FD_ZERO(&standard_input);
                 close(filedes[1]);
                 while(1)
                 {
-                        kbd_input = 0;
-                        FD_SET(0, &standard_input);
-                        select(1, &standard_input, NULL, NULL, NULL);
-                        if (FD_ISSET(0, &standard_input))
-                        {
-                                read(0, &kbd_input, sizeof(int));
-                                if (kbd_input == 1)
-                                {
-                                        return -1;
-                                }
-                        }        
+//                        kbd_input = 0;
+//                        FD_SET(0, &standard_input);
+//                        select(1, &standard_input, NULL, NULL, NULL);
+//                        if (FD_ISSET(0, &standard_input))
+//                        {
+//                                read(0, &kbd_input, sizeof(int));
+//                                if (kbd_input == 1)
+//                                {
+//                                        return -1;
+//                                }
+//                        }        
 
                         read(filedes[0], &single_char, sizeof(char));
                         cout << single_char << endl;
