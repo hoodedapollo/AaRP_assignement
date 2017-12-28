01e801fc8843f4271ad38e5f26398613743e1806
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-28 13:26:06 +0100

    Add comments

--- secondAssign/auxiliary_functions.h
+++ secondAssign/auxiliary_functions.h
@@ -121,25 +121,25 @@ void int_2Darray_dynamic_alloc(int ** table, int rows, int columns) // Dynamic a
 // }
 
 
-char** fromVecToArrayOfStringFds(std::vector<std::vector<std::vector<int> > > filedes_3Dtable)
+char** fromVecToArrayOfStringFds(std::vector<std::vector<std::vector<int> > > filedes_3Dtable) // convert a 3Dvector of file descriptors into an array of string where each string is a file descriptor, first two element of the array are the number of subscribers and the number of publishers 
 {
-        int subs_num = filedes_3Dtable.size();
-        int pubs_num = filedes_3Dtable[0].size();
+        int subs_num = filedes_3Dtable.size(); // number of rows of the table corresponds to the number of subscribers
+        int pubs_num = filedes_3Dtable[0].size(); // number of columns of the table corresponds to the number of publishers
         char** array; 
-        int array_length = (2 + 2*(subs_num * pubs_num));
+        int array_length = (2 + 2*(subs_num * pubs_num)); // allocate memory dynamically to store all the file descriptors + two element for the number of subscribers and the number of publishers
         array = new char*[array_length];
         for (int i = 0; i < array_length; i++)
         {
                 array[i] = new char[NCHAR_FOR_INT];
         }
-        sprintf(array[0], "%d", subs_num);
-        sprintf(array[1], "%d", pubs_num);
-        int q = 2; 
+        sprintf(array[0], "%d", subs_num); // store the number of subscribers in the first element of the array 
+        sprintf(array[1], "%d", pubs_num); // store the number of subscribers in the first element of the array 
+        int q = 2; // start to store the file descriptors from the third element since the first two are no more available 
         for (int i = 0; i < subs_num; i++)
         {
             for (int j = 0; j < pubs_num; j++)
             {
-                    sprintf(array[q],"%d", filedes_3Dtable[i][j][0]);
+                    sprintf(array[q],"%d", filedes_3Dtable[i][j][0]); // store the reading file descriptor converting it into a string
                     q++;
             }
         }
@@ -148,7 +148,7 @@ char** fromVecToArrayOfStringFds(std::vector<std::vector<std::vector<int> > > fi
         {
             for (int j = 0; j < pubs_num; j++)
             {
-                    sprintf(array[q],"%d", filedes_3Dtable[i][j][1]);
+                    sprintf(array[q],"%d", filedes_3Dtable[i][j][1]); // store the writing file descriptor converting it into a string
                     q++;
             }
         } 
@@ -156,9 +156,9 @@ char** fromVecToArrayOfStringFds(std::vector<std::vector<std::vector<int> > > fi
 }
 
 
-std::vector<std::vector<std::vector<int> > > fromCharToVecFds(char* filedes[])
+std::vector<std::vector<std::vector<int> > > fromCharToVecFds(char* filedes[]) // build a 3Dvector of fildescriptors from an array which stores th number of subscribers and publishers and all the file descriptors
 {
-       int subs_num = atoi(filedes[0]);
+       int subs_num = atoi(filedes[0]); 
        int pubs_num = atoi(filedes[1]);
 
        std::vector<int> fd_row(2,0);
