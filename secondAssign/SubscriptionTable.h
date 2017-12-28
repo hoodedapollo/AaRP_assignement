4158917b5d466c11301d4f9c895c576c802a468c
Marco Ruzzon <marco.ruzzon@mail.polimi.it>
2017-12-28 15:46:46 +0100

    add a new constructor without paramenters

--- secondAssign/SubscriptionTable.h
+++ secondAssign/SubscriptionTable.h
@@ -17,6 +17,7 @@ class SubscriptionTable
                 vector<vector<vector<int> > > vector_data_filedes;
                 vector<vector<vector<int> > > vector_notify_filedes;
         public:
+                SubscriptionTable();
                 SubscriptionTable(int num_of_subs, int num_of_pubs); // generate a table in which is specified which subscriber is subscribed to which pulisher's topic by asking the user for information, paramenters are the number of publishers and subscribers
                 SubscriptionTable(vector<vector<vector<int> > > matching_table); // given the matching table as a paramenter is sets the number of subscribers as the number of rows of the table and the number of publishers as the number of columns of the first row of the table
                 vector<vector<int> >getTable(); // returns the matching table
@@ -25,6 +26,42 @@ class SubscriptionTable
                 vector<vector<vector<int> > > generateNotifyPipes(); // generate the table of notify pipes file descriptors based on the matching table built by the constructor
 
 };
+
+
+SubscriptionTable::SubscriptionTable() // generate a table in which is specified which subscriber is subscribed to which pulisher's topic by asking the user for information, paramenters are the number of publishers and subscribers
+{ 
+        string answer;
+
+        cout << "How many publisher do you want?" << endl;
+        cin >> pubs_num;
+        cout << "How many subscriber do you want?" << "\n\n";
+        cin >> subs_num;
+
+        // table dynamic memory allocation
+        table.resize(subs_num);
+        for(int i = 0; i < subs_num; i++)
+        {
+                table[i].resize(pubs_num,0);
+        } 
+        
+        // build the table asking the user for information
+        for ( int i = 0; i < subs_num; i++ )
+        { 
+                for(int j = 0; j < pubs_num; j++)
+                {
+                        cout << "Is subscriber " << i << "subscribed to topic published by publisher " << j << "?\n"; 
+                        cin >> answer;
+                        if (answer.compare("y") == 0 || answer.compare("yes") ==0)
+                        {
+                                table[i][j] = 1;
+                        }
+                        else
+                        {
+                                table[i][j] = 0;
+                        }
+                }
+        }   
+}
 
 SubscriptionTable::SubscriptionTable(int num_of_subs, int num_of_pubs) // generate a table in which is specified which subscriber is subscribed to which pulisher's topic by asking the user for information, paramenters are the number of publishers and subscribers
 { 
